da8ce21d62bc3d59d6e6299dbc5b2736d64eccce
Danno Ferrin (shemnon) <danno.ferrin@shemnon.com>
2018-09-15 22:23:38 -0600

    Fix assignment bug, other lint issues.

--- gcp/index.js
+++ gcp/index.js
@@ -1,7 +1,11 @@
+/*jslint node: true */
+/*jshint esversion: 6 */
+"use strict";
+
 var config = require("config");
-var fs = require("fs")
-var Web3 = require("web3")
-const { IncomingWebhook } = require('@slack/client');
+var fs = require("fs");
+var Web3 = require("web3");
+var { IncomingWebhook } = require('@slack/client');
 
 const url = config.get("contracts.DAI.slack_webhook_url");
 const webhook = new IncomingWebhook(url);
@@ -11,37 +15,38 @@ var daiAbi = JSON.parse(fs.readFileSync('daiAbi.json'));
 var web3 = new Web3();
 
 function decodeEvent(event, body) {
-    var topics = event.inputs.filter(v => v.indexed)
-    var data = event.inputs.filter(v => !v.indexed)
+    var topics = event.inputs.filter(v => v.indexed);
+    var data = event.inputs.filter(v => !v.indexed);
 
     var decodableTopics = body.topics.slice(1);
 
-    var eventParams = {}
-    for (var i in topics) {
-        var value = web3.eth.abi.decodeParameter(topics[i].type, decodableTopics[i])
+    var i;
+    var eventParams = {};
+    for (i in topics) {
+        var value = web3.eth.abi.decodeParameter(topics[i].type, decodableTopics[i]);
         eventParams[topics[i].name] =  value;
     }
 
-    var dataValues = web3.eth.abi.decodeParameters(data.map(v => v.type),  body.data)
-    for (var i = 0; i < data.length; i++) {
-        eventParams[data[i].name] = dataValues[i]
+    var dataValues = web3.eth.abi.decodeParameters(data.map(v => v.type),  body.data);
+    for (i = 0; i < data.length; i++) {
+        eventParams[data[i].name] = dataValues[i];
     }
 
     return eventParams;
 }
 
 function decodeEventFromAbi(abi, encodedEvent) {
-    var eventsByKey = {}
+    var eventsByKey = {};
     for (var index in abi) {
-        var entry = abi[index]
+        var entry = abi[index];
         if (entry.type == 'event') {
             var key = web3.eth.abi.encodeEventSignature(entry);
-            eventsByKey[key] = entry
+            eventsByKey[key] = entry;
         }
     }
     
-    var event = eventsByKey[encodedEvent.topics[0]]
-    return {"name":event.name, "data":decodeEvent(event, encodedEvent)}
+    var event = eventsByKey[encodedEvent.topics[0]];
+    return {"name":event.name, "data":decodeEvent(event, encodedEvent)};
 }
 
 function wadToUSD(wad) {
@@ -50,26 +55,27 @@ function wadToUSD(wad) {
 
 
 exports.http = (request, response) => {
-  var value = decodeEventFromAbi(daiAbi, request.body)
+  var value = decodeEventFromAbi(daiAbi, request.body);
+  var amount;
 
-  var channel = "#test"
+  var channel = "#test";
   var text = "Unhandleded << " + JSON.stringify(value) + " >>";
 
   if (value.name == 'Transfer') {
-    channel = "#teller"
-    var amount = wadToUSD(value.data.wad)
+    channel = "#teller";
+    amount = wadToUSD(value.data.wad);
     text = "💵💵 " + amount + " 💵💵 DAI from:" + value.data.src + " to:" + value.data.dst;
-  } else if (value.name = 'Mint') {
-    channel = "#vault"
-    var amount = wadToUSD(value.data.wad)
+  } else if (value.name == 'Mint') {
+    channel = "#vault";
+    amount = wadToUSD(value.data.wad);
     text = "💰💰 " + amount + " 💰💰 DAI created from thin air for " + value.data.guy;
-  } else if (value.name = 'Burn') {
-    channel = "#vault"
-    var amount = wadToUSD(value.data.wad)
+  } else if (value.name == 'Burn') {
+    channel = "#vault";
+    amount = wadToUSD(value.data.wad);
     text = "🔥🔥 " + amount + " 🔥🔥 DAI burnt by the pyro at " + value.data.guy;
-  } else if (value.name = 'Approval') {
-    channel = "#willcall"
-    var amount = wadToUSD(value.data.wad)
+  } else if (value.name == 'Approval') {
+    channel = "#willcall";
+    amount = wadToUSD(value.data.wad);
     text = "💳💳 " + amount + " 💳💳 DAI to be spent from " + value.data.src + " by " + value.data.guy;
   }
 
